7910efb2dbbbd29a3807ff953d182494d248f040
SofiMi <maslennikova.si@phystech.edu>
2022-05-10 19:13:48 +0300

    Добавлены ответы на вопрос

--- commands/find_in_old_quest.py
+++ commands/find_in_old_quest.py
@@ -4,7 +4,7 @@ from create import dp, bot
 from keyboards.keyboard_find import keyboard_find
 from keyboards.keyboard_find import support_callback
 from keyword_search.search import search_message
-from keyboards.keyboard_find_key import keyboard_find_key
+from keyboards.keyboard_find_key import keyboard_find_key, keyboard_find_key_s
 from sql.sql_handling import SQL
 
 @dp.message_handler(Command("find"))
@@ -25,6 +25,7 @@ async def click_on_the_button_find(call, state, callback_data):
 
 @dp.message_handler(state="find_message")
 async def get_support_message_find(message, state):
+    "Функция, которая выводит потенциально нужные вопросы из базы данных"
     sql = SQL("sql/faq.db")
     text_n = str(message.text)
     keys = []
@@ -33,13 +34,22 @@ async def get_support_message_find(message, state):
         keys.append(key[0])
     new_keys = set(await search_message(text_n)) & set(keys)
     list_ques = []
+    list_answ = []
     for n_key in new_keys:
         for old_key in sql_key:
             if n_key == old_key[0]:
                 list_ques.append(old_key[1])
-
-    keyboard = await keyboard_find_key("find_quest", list_ques)
-    await message.answer("Вас интересует что-то из этих вопросов?", reply_markup=keyboard)
+                list_answ.append(old_key[2])
+    keyboard = await keyboard_find_key("find_key", list_ques, list_answ)
+    await message.answer("Вас интересует что-то из этих вопросов? Если вы не нашли нужную информацию, то "
+                         "нажмите /support или /support_call", reply_markup=keyboard)
 
     await state.reset_state()
     sql.close()
+
+@dp.callback_query_handler(keyboard_find_key_s.filter(messages="find_key"))
+# Декоратор оборачивает функцию, когда нажимается кнопка с отправкой одного сообщения модератору
+async def click_on_ques(call, callback_data):
+    """Функция, которая обрабатывает нажатие на кнопку отправки сообщения"""
+    await call.answer()
+    await call.message.answer(f"{callback_data.get('answer')}")
--- keyboards/keyboard_find_key.py
+++ keyboards/keyboard_find_key.py
@@ -1,17 +1,18 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 
-support_callback = CallbackData("ask_support", "messages")
+keyboard_find_key_s = CallbackData("ask_support", "messages")
 
-async def keyboard_find_key(messages, list):
+async def keyboard_find_key(messages, list, list_answer):
     """Функция, которая передает клавиатуру find"""
     keyboard = InlineKeyboardMarkup()
     for i in range(len(list)):
         keyboard.add(
             InlineKeyboardButton(
                 text=list[i],
-                callback_data=support_callback.new(
-                    messages=messages
+                callback_data=keyboard_find_key_s.new(
+                    messages=messages,
+                    answer=list_answer[i]
                 )
             )
         )
